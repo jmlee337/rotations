5dfa4fdd6aa496464c3785900f3b33d4a183d2d0
Jeremy Lee <jmlee337@gmail.com>
2015-10-24 14:55:33 -0700

    add selector between current and all, and styles

--- client/client.js
+++ client/client.js
@@ -1,7 +1,35 @@
 Meteor.subscribe("items");
 Meteor.subscribe("preferences");
 
+Template.itemsView.helpers({
+  showAll: function() {
+    return Session.get("showAll");
+  }
+});
+
+Template.itemsSelector.helpers({
+  showAll: function() {
+    return Session.get("showAll");
+  }
+});
+
+Template.itemsSelector.events({
+  'click .current': function() {
+    Session.set("showAll", false);
+  },
+
+  'click .all': function() {
+    Session.set("showAll", true);
+  }
+});
+
 Template.allItems.helpers({
+  items: function() {
+    return Items.find({}, {sort: [["_id", "asc"]]});
+  }
+});
+
+Template.currentItems.helpers({
   items: function() {
     return Items.find({}, {sort: [["_id", "asc"]]});
   }
