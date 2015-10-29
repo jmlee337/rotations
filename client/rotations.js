f4eb25e363a0233d7eb3f9eeed4784753062dc11
Jeremy Lee <jmlee337@gmail.com>
2015-10-29 02:11:13 -0700

    whoops. a lot of stuff. adding items, removing items, event handler for editing items, meteor updated

--- client/rotations.js
+++ client/rotations.js
@@ -8,9 +8,44 @@ Accounts.onLogin(function() {
   }
 });
 
+Template.body.helpers({
+  view: function() {
+    return Session.get("view");
+  }
+});
+
+Template.addView.events({
+  'click .back': function() {
+    Session.set("view", "itemsView");
+  },
+
+  'submit form': function(event) {
+    event.preventDefault();
+    var title = event.target.newTitle.value;
+    var body = event.target.newBody.value;
+    if (!body) {
+      return;
+    }
+
+    if (title) {
+      Meteor.call("insertItemWithTitle", body, title);
+    } else {
+      Meteor.call("insertItem", body);
+    }
+
+    Session.set("view", "itemsView");
+  }
+});
+
 Template.itemsView.helpers({
   showAll: function() {
     return Session.get("showAll");
+  }
+});
+
+Template.itemsView.events({
+  'click .fab': function() {
+    Session.set("view", "addView");
   }
 });
 
@@ -33,6 +68,16 @@ Template.itemsSelector.events({
 Template.allItems.helpers({
   items: function() {
     return Items.find({}, {sort: [["_id", "asc"]]});
+  }
+});
+
+Template.allItems.events({
+  'click .delete': function(event) {
+    deleteItem(event.currentTarget.value);
+  },
+
+  'click .edit': function(event) {
+    editItem(event.currentTarget.value);
   }
 });
 
@@ -59,3 +104,21 @@ Template.currentItems.helpers({
     }
   }
 });
+
+Template.currentItems.events({
+  'click .delete': function(event) {
+    deleteItem(event.currentTarget.value);
+  },
+
+  'click .edit': function(event) {
+    editItem(event.currentTarget.value);
+  }
+});
+
+deleteItem = function(id) {
+  Meteor.call("removeItem", id);
+};
+
+editItem = function(id) {
+  console.log("edit: " + id);
+};
